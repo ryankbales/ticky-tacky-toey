a23bbc5716ff5126d86efe397b9ff9ba78e1d120
Ryan Bales <ryankbales@gmail.com>
2014-10-02 12:11:17 -0700

    reframing

--- the_game.rb
+++ the_game.rb
@@ -1,6 +1,26 @@
-board = "
+#create board
+#introduce the game
+#assign x or o to respective player
+#ask player for input
+#assing input to board
+#get computer input
+#assign input to board
+#checks for winner
+#if no winner ask for input again
+#if there is a winner or the board is full announce outcome
+
+def create_empty_board
+
+end
+
+def assign_player_token
+end
+
+def get_input
+end
+
+def check_for_winner
+end
 
 
-
-"
-puts board
+puts "Lets play Tic Tac Toe!"
