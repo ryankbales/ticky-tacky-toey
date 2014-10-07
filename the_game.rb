55966fd949ef7980db3d6f4c2f0eb6c1289b5ce8
Ryan Bales <ryankbales@gmail.com>
2014-10-06 23:46:44 -0700

    scope seems to be an issue

--- the_game.rb
+++ the_game.rb
@@ -9,11 +9,29 @@ require 'pry'
 #checks for winner
 #if no winner ask for input again
 #if there is a winner or the board is full announce outcome
-
 def grid
   g = {}
-  (1..9).each {|pos| g[pos] = " "}
+  i = 1
+  (1..9).each do |pos|
+    g[pos] = i
+    i += 1
+  end
   g
+end
+
+winning_sets = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+puts "Let's play tic tac toe!"
+spots = grid
+human = ""
+machine = ""
+outcome = ""
+status = false
+puts "Do you want to be X or O"
+human = gets.chomp.upcase
+if human == 'X'
+  machine = 'O'
+else
+  machine = 'X'
 end
 
 def make_grid(g)
@@ -25,16 +43,61 @@ def make_grid(g)
   puts " #{g[7]} | #{g[8]} | #{g[9]} | "
 end
 
-def pick_your_marker
-
+def there_is_a_win_or_draw?(current_set, player_1, player_2, outcome, result)
+  current_set.each do |a|
+    if a.uniq.length == 1 #If all items are the same
+      if a.first == player_1
+        outcome = "#{player_1.upcase} is the winner!"
+        result = true
+      else
+        outcome = "#{player_2.upcase} is the winner!"
+        result = true
+      end
+    elsif  (a.uniq.length > 1) && (!a.include?(Integer)) #if they aren't the same it's a tie
+      outcome = "It's a draw!"
+      result = true
+    else
+      outcome = "Select again."
+      result = false
+    end
+  end
 end
 
-def x_marks_spot(x)
-  puts "X, mark your spot(1-9)"
+while status == false
+  make_grid(spots)
+  puts "#{human}, mark your spot!(1-9)" # get human input
   spot = gets.chomp.to_i
-  x[spot] = "X"
+
+  if (spots[spot] != "X") || (spots[spot] != "O")
+    spots[spot] = human #assign to board and replace number in winning_set
+    winning_sets.each do |i|
+      i.each do |mark|
+        if mark == spot
+          mark = human
+        end
+      end
+    end
+  end
+
+  #do this again for computer, except for the selection part
+  available_spots = spots.select { |k, v|  v.is_a?(Integer)  }
+  available_spots_value = []
+  available_spots.each do |k, v|
+    available_spots_value << v
+  end
+
+  machine_choice = available_spots_value.shuffle.first
+  if (spots[machine_choice] != "X") || (spots[machine_choice] != "O")
+    spots[machine_choice] = machine #assign to board and replace number in winning_set
+    winning_sets.each do |i|
+      i.each do |mark|
+        if mark == machine_choice
+          mark = machine
+        end
+      end
+    end
+  end
+
+  there_is_a_win_or_draw?(winning_sets, human, machine, outcome, status)
+  puts outcome
 end
-
-spots = grid
-
-make_grid(spots)
